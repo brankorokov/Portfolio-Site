5691e7468caf2618cce29c3324b9f3cb5dfc73f7
Branko Rokov <brankorokov@outlook.com>
2020-06-06 16:34:20 -0400

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
